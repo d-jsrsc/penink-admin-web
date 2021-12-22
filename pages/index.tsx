2300687b98c41191ed44e23db09849cc9e07ad57
d_jsrsc <d_jsrsc@163.com>
2021-12-22 14:40:49 +0800

    some

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,42 @@
 import type { NextPage } from "next";
 import useSWR from "swr";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+import axios from 'axios'
+import { useState, useEffect } from 'react';
+
+const fetcher = (url: string) => axios.get(url).then(res => res.data)
+
+const storyPass = async (id: string) => await axios.put(`/api/story`, {
+  publishStatus: '',
+})
+
+
+function useNextStory (id: string | undefined) {
+  const { data, error } = useSWR(`/api/story?id=${id}`, fetcher)
+
+  return {
+    story: data,
+    isLoading: !error && !data,
+    isError: error
+  }
+}
 
 const Home: NextPage = () => {
-  const { data, error } = useSWR("/api/hello", fetcher);
+  const [ currId, setCurrId ] = useState(1);
+  const { story, isLoading, isError } = useNextStory(currId + '');
 
-  if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
-  return <div>hello {data.name}!</div>;
+
+  if (isError) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+  return (
+    <div>
+      <div><button onClick={async () => {
+        await storyPass('haha');
+        setCurrId( currId + 1)
+      }}>pass</button><button>next</button></div>
+      <div>hello {story.name}!</div>
+    </div>
+  );
 };
 
 export default Home;
